960df10c2d29d3e5003bea0896e859219eecdaaa
saumittras <saumittra.eee@gmail.com>
2024-10-27 21:50:18 +0600

    first commit and added tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-4xl text-center">Vite + React</h1>
+      <button className="btn">Button</button>
+    </>
+  );
+}
+
+export default App;
